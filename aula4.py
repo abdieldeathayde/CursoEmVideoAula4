b7cd70143a7e7a74368371f76a6061d5d796b794
Abdiel Athayde <abdieldeathayde67@gmail.com>
2022-08-25 08:53:09 -0300

    feat add exercises class 4

---
+++ aula4.py
@@ -0,0 +1,24 @@
+nome = input('Digite seu nome: \n')
+idade = int(input('Digite sua idade:'))
+peso = float(input('Digite seu peso:'))
+
+print(nome, idade, peso)
+
+#Desafio 1
+nome = input('Qual é o seu nome? \n')
+print(f"Olá {nome}! Prazer em te conhecer!")
+
+#Desafio 2 
+Dia = int(input('Dia = '))
+Mes = input('Mes = ')
+Ano = int(input('Ano = '))
+
+print(f"Você nasceu no dia {Dia} de {Mes} de {Ano} . Correto?")
+
+# Desafio 3
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite outro numero: '))
+
+soma = numero1 + numero2
+
+print(f"A soma é: {soma}")
